f121bc0b1e372ab989ee8ba1399d62c0b73607e3
Tanmay <tanmaychoudhary4102@gmail.com>
2024-01-12 22:10:42 +0530

    commiit

--- app.js
+++ app.js
@@ -26,10 +26,12 @@ app.use(cors());
 const kanbanRouter = require("./routes/kanban");
 const deleteAllRouter = require("./routes/deleteAll"); // Correct path to the router file
 const tasks = require("./routes/tasks"); // Correct path to the router file
+const weather = require("./routes/weather"); // Correct path to the router file
 
 app.use("/kanban", kanbanRouter);
 app.use("/deleteAll", deleteAllRouter); // Correct mounting point
 app.use("/tasks", tasks); // Correct mounting point
+app.use("/weather", weather);
 
 app.listen(8080, () => {
   console.log("Server started on port 8080");
---
+++ routes/weather.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const router = express.Router();
+const api = {
+  key: "2e771d144142dcd2ccd8e9afdf85f3a3",
+  base: "http://api.openweathermap.org/data/2.5/",
+};
+const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+router.get("/", async (req, res) => {
+  try {
+    //const kanban = await kanbanModel.find();
+    const lat = 55;
+    const lon = -3; // Replace with the actual longitude
+    const apiKey = api.key; // Replace with your actual API key
+
+    const kanban = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=delhi&limit=5&appid=2e771d144142dcd2ccd8e9afdf85f3a3`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((result) => {
+        return result;
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+      });
+    res.json(kanban);
+  } catch (err) {
+    res.send("Error " + err);
+  }
+});
+
+router.get("/get-weather", async (req, res) => {
+  try {
+    //const kanban = await kanbanModel.find();
+    const lat = 28.6517178;
+    const lon = 77.2219388; // Replace with the actual longitude
+    const apiKey = api.key; // Replace with your actual API key
+
+    const kanban = await fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((result) => {
+        return result;
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+      });
+    res.json(kanban);
+  } catch (err) {
+    res.send("Error " + err);
+  }
+});
+module.exports = router;
